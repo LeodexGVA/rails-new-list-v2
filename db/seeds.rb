4f2843ba7c6e630955ea6e0c29ab3bec585ce349
Dylan Cianci <cianci.dy@gmail.com>
2023-08-26 18:46:29 +0200

    Création d'une seed avec API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,18 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-
+require "json"
 # Permet de nettoyer la DB à chaque pousser de seed
 Movie.destroy_all
+Movie.destroy_all
+List.destroy_all
+
+# Message d'affichage
 puts "Cleaning DB"
+puts "Creating movies..."
 
-Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
-Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
-Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
-Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+tmdb_url = "http://tmdb.lewagon.com/movie/top_rated?api_key=d5b8dd28dbdc765030324d1fdfdb1cde"
 
-puts "New DB ready."
+# Effectuer une requête GET et récupérer les données JSON
+response = URI.open(tmdb_url)
+movies_data = JSON.parse(response.read)
+
+# Créer un fichier de vue pour formater les données JSON
+jbuilder_template = Jbuilder.new do |json|
+  json.array! movies_data['results'] do |movie_data|
+    json.title movie_data['title']
+    json.overview movie_data['overview']
+    json.poster_url "https://image.tmdb.org/t/p/original#{movie_data['poster_path']}"
+    json.rating movie_data['vote_average']
+  end
+end
+
+# Créer des films à partir des données formatées
+jbuilder_result = JSON.parse(jbuilder_template.target!)
+jbuilder_result.each do |movie_data|
+  Movie.create!(
+    title: movie_data['title'],
+    overview: movie_data['overview'],
+    poster_url: movie_data['poster_url'],
+    rating: movie_data['rating']
+  )
+end
+puts "Finished!"
